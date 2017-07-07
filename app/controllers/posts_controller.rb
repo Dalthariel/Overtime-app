68fc8f7d4ab5ded4c414f22046d8a837bdd97780
Dalthariel <david.hamaker@gmail.com>
2017-07-06 20:57:48 -0600

    Implemented ability to delete post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show, :edit, :update]
-
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	
 	def index
-		@posts= Post.all
+		@posts = Post.all
 	end
 
 	def new
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 		@post.user_id = current_user.id
 
 		if @post.save
-			redirect_to @post, notice: "Your post was created successfully"
+			redirect_to @post, notice: 'Your post was created successfully'
 		else
 			render :new
 		end
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	def update
 		if @post.update(post_params)
-			redirect_to @post, notice: "Your post was updated successfully"
+			redirect_to @post, notice: 'Your post was edited successfully'
 		else
 			render :edit
 		end
@@ -34,13 +34,18 @@ class PostsController < ApplicationController
 	def show
 	end
 
+	def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
+	end
+
 	private
 
-		def post_params
-			params.require(:post).permit(:date, :rationale)
-		end
+	  def post_params
+	  	params.require(:post).permit(:date, :rationale)
+	  end
 
-		def set_post
-			@post = Post.find(params[:id])
-		end
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,11 +15,11 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
 
-    it "has a title of Posts" do
-    	expect(page).to have_content(/Posts/)
+    it 'has a title of Posts' do
+      expect(page).to have_content(/Posts/)
     end
 
-    it "has a list of posts" do
+    it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
@@ -28,38 +28,48 @@ describe 'navigate' do
   end
 
   describe 'new' do
-    it "has a link from the home page" do
+    it 'has a link from the homepage' do
       visit root_path
+
       click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
     end
   end
 
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
-  	before do
-  		
-  		visit new_post_path
-  	end
+    before do
+      visit new_post_path
+    end
 
-  	it "has a new form that can be reached" do
-  		expect(page.status_code).to eq(200)
-  	end
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'can be created from new form page' do
-		  fill_in 'post[date]', with: Date.today
-		  fill_in 'post[rationale]', with: "Some rationale"
-		  click_on "Save"
-		  expect(page).to have_content("Some rationale")
-		end
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
 
-		it "will have a user associated with it" do
-			fill_in 'post[date]', with: Date.today
-		  fill_in 'post[rationale]', with: "User Association"
-		  click_on "Save"
+      expect(page).to have_content("Some rationale")
+    end
 
-		  expect(User.last.posts.last.rationale).to eq("User Association")
-		end
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
 
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
   end
 
   describe 'edit' do
@@ -67,15 +77,14 @@ describe 'navigate' do
       @post = FactoryGirl.create(:post)
     end
 
-    it "can be reached by clicking edit on index page" do
-      
+    it 'can be reached by clicking edit on index page' do
       visit posts_path
 
       click_link("edit_#{@post.id}")
       expect(page.status_code).to eq(200)
     end
 
-    it "can be edited" do
+    it 'can be edited' do
       visit edit_post_path(@post)
 
       fill_in 'post[date]', with: Date.today
@@ -83,6 +92,6 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Edited content")
-    end  
-  end  
+    end
+  end
 end
